e7172836ab0ebabe69ce04322bfa9679ebeb2efc
ankurpatel-india <ankurishan24@gmail.com>
2023-09-16 23:31:43 +0300

    data ingestion development is completed

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from src.MLDS.logger import logging
 from src.MLDS.exception import CustomException
 import sys
-
+from src.MLDS.components.data_ingestion import DataIngestion
+from src.MLDS.components.data_ingestion import DataIngestionConfig
 if __name__=="__main__":
     logging.info("The exectution has started.") 
     try:
-        a=1/0
+        #data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
     except Exception as e:
         logging.info("Custome Exceptions")
         raise CustomException(e,sys)                                                    
